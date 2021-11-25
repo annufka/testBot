bcca5666313c5ffd2c5bd2c41d80f4d8286a487b
annufka <annufka94@gmail.com>
2021-11-25 14:23:37 +0200

    webhooks done!)

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-
+web: gunicorn main:server
 web: python3 main.py
--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import os
 
+import flask
 import telebot
 from flask import Flask, request
 
-from data.config import BOT_TOKEN, PORT
+from data.config import BOT_TOKEN
 from utils.db_api.sqlite import Database
 from utils.start_notify import on_startup_notify
 
 bot = telebot.TeleBot(BOT_TOKEN)
 db = Database(path_to_db="data/main.db")
-# server = Flask(__name__)
+server = Flask(__name__)
 
 
-# @server.route('/' + BOT_TOKEN, methods=['POST', 'GET'])
-# def getMessage():
-#     json_string = request.get_data().decode('utf-8')
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])
-#     return "!", 200
-#
-#
-# @server.route("/")
-# def webhook():
-#     bot.delete_webhook()
-#     bot.set_webhook(url='https://agile-ridge-52994.herokuapp.com/' + BOT_TOKEN)
-#     return "!", 200
+@server.route('/' + BOT_TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route('/', methods=["GET"])
+def index():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://agile-ridge-52994.herokuapp.com/{}".format(BOT_TOKEN))
+    return "Hello from Heroku!", 200
 
 
 if __name__ == '__main__':
@@ -35,8 +34,8 @@ if __name__ == '__main__':
     db.create_table_users()
 
     # уведомление админам
-    # on_startup_notify(bot)
+    on_startup_notify(bot)
 
-    bot.infinity_polling(skip_pending=True)
-    # server.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', '5000')))
+    # bot.infinity_polling(skip_pending=True)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     # вернуть web: gunicorn main:server в Procfile если webhook
